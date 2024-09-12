146215884adff427d50a60e1ef170dd3edb133b1
AmanPahadia <134621518+AmanPahadia@users.noreply.github.com>
2024-09-13 04:07:16 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -49,7 +49,7 @@ function Home() {
             image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
           />
         </div>
-
+//second
         <div className="home__row">
           <Product
             id="90829332"
